23c473bee3ccd99371e84166c4af3950e2c583bb
qwerty31-15 <sadpoyo112@gmail.com>
2025-02-22 22:06:00 +0500

    Update bootloader.asm

--- kernel/boot/bootloader.asm
+++ kernel/boot/bootloader.asm
@@ -13,52 +13,151 @@ start:
     int 0x10        ; Вызов BIOS
 
     ; Установка курсора в верхний левый угол
-    mov ah, 0x02    ; Функция установки курсора
-    mov bh, 0x00    ; Страница 0
-    mov dx, 0x0000  ; Строка 0, столбец 0
-    int 0x10        ; Вызов BIOS
+    mov ah, 0x02
+    mov bh, 0x00
+    mov dx, 0x0000
+    int 0x10
 
     ; Вывод логотипа StableEMM
-    mov si, logo    ; Загружаем адрес логотипа
+    mov si, logo
     call print_string
 
     ; Сообщение о загрузке
     mov si, loading_msg
     call print_string
 
-    ; Завершение (в реальной ОС здесь будет загрузка ядра)
+    ; Запрос логина
+    mov si, username_prompt
+    call print_string
+    mov di, username
+    call get_input
+
+    ; Запрос пароля
+    mov si, password_prompt
+    call print_string
+    mov di, password
+    call get_password_input
+
+    ; Проверка аутентификации
+    call authenticate
+    cmp ax, 0
+    je auth_failed
+
+    ; Переход к ядру (эмуляция)
     jmp $
 
+auth_failed:
+    mov si, auth_failed_msg
+    call print_string
+    jmp $
+
+; =============================
 ; Функция для вывода строки
 print_string:
-    mov ah, 0x0E    ; Функция BIOS для вывода символа
+    mov ah, 0x0E
 .print_char:
-    lodsb           ; Загружаем следующий символ из строки в AL
-    cmp al, 0       ; Проверяем, не конец ли строки (0)
-    je .done        ; Если конец, завершаем
-    int 0x10        ; Выводим символ на экран
-    jmp .print_char ; Повторяем для следующего символа
+    lodsb
+    cmp al, 0
+    je .done
+    int 0x10
+    jmp .print_char
 .done:
     ret
 
-; Функция задержки
-delay:
-    push cx
-    mov cx, 0x0FFF  ; Значение для задержки
-.delay_loop:
-    loop .delay_loop
-    pop cx
+; Функция для ввода строки
+get_input:
+    xor cx, cx          ; Счётчик символов
+.get_char:
+    mov ah, 0x00        ; Ожидаем нажатие клавиши
+    int 0x16
+    cmp al, 0x0D        ; Проверка на Enter
+    je .done
+    mov [di], al        ; Сохраняем символ
+    inc di
+    inc cx
+    mov ah, 0x0E        ; Печатаем введённый символ
+    int 0x10
+    jmp .get_char
+.done:
+    mov byte [di], 0    ; Завершаем строку
     ret
 
+; Функция для ввода пароля (скрытый ввод)
+get_password_input:
+    xor cx, cx
+.get_pass_char:
+    mov ah, 0x00
+    int 0x16
+    cmp al, 0x0D
+    je .done
+    mov [di], al
+    inc di
+    inc cx
+    mov ah, 0x0E
+    mov al, '*'
+    int 0x10
+    jmp .get_pass_char
+.done:
+    mov byte [di], 0
+    ret
+
+; Функция аутентификации
+authenticate:
+    mov si, correct_username
+    mov di, username
+    call strcmp
+    jne auth_fail
+
+    mov si, correct_password
+    mov di, password
+    call strcmp
+    jne auth_fail
+
+    mov ax, 1  ; Успех
+    ret
+
+auth_fail:
+    mov ax, 0  ; Ошибка
+    ret
+
+; Функция сравнения строк
+strcmp:
+.compare_loop:
+    mov al, [si]
+    mov bl, [di]
+    cmp al, bl
+    jne .not_equal
+    cmp al, 0
+    je .equal
+    inc si
+    inc di
+    jmp .compare_loop
+
+.not_equal:
+    mov ax, 1
+    ret
+
+.equal:
+    xor ax, ax
+    ret
+
+; =============================
 ; Данные
-logo db "   _____ _        _     _      _____ __  __ __  __ ", 0x0D, 0x0A,
-     db "  / ____| |      | |   | |    | ____|  \/  |  \/  |", 0x0D, 0x0A,
-     db " | (___ | |_ __ _| |__ | | ___| |___| \  / | \  / |", 0x0D, 0x0A,
-     db "  \___ \| __/ _` | '_ \| |/ _ \  ___| |\/| | |\/| |", 0x0D, 0x0A,
-     db "  ____) | || (_| | |_) | |  __/ |___| |  | | |  | |", 0x0D, 0x0A,
-     db " |_____/ \__\__,_|_.__/|_|\___|_____|_|  |_|_|  |_|", 0x0D, 0x0A, 0
 
-loading_msg db "Loading StableEMM...", 0
+logo db "Welcome to StableEMM", 0x0D, 0x0A, 0
 
-times 510-($-$$) db 0  ; Заполняем оставшуюся часть сектора нулями
-dw 0xAA55              ; Сигнатура загрузочного сектора
+loading_msg      db "Loading StableEMM...", 0
+username_prompt  db "Enter username: ", 0
+password_prompt  db "Enter password: ", 0
+auth_failed_msg  db "Authentication failed!", 0
+
+username         db 20 dup(0)
+password         db 20 dup(0)
+
+correct_username db "root", 0
+correct_password db "root", 0
+
+; =============================
+; Завершение загрузочного сектора
+times 510 - ($ - $$) db 0
+dw 0xAA55
